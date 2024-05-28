00235a4654cdbdaa9ea8f146270d43c5fbdae329
GProgrammerN <guilherme.programador12@gmail.com>
2024-05-28 18:09:00 -0300

    Removed the other pages

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function revelarSimulado(materia) {
+    switch (materia) {
+        case 'engSoftware':
+            document.getElementById('formEng').style.display = 'block';
+            break;
+        case 'bancoDados':
+            document.getElementById('formBD').style.display = 'block';
+            break;
+        case 'poo':
+            document.getElementById('formPOO').style.display = 'block';
+            break;
+    }
+
+}
+
 function verificarRespostas(materia) {
     var acertos = 0;
     var totalQuestoes = 5;
@@ -6,17 +21,23 @@ function verificarRespostas(materia) {
     switch (materia) {
         case 'engSoftware':
             respostasCorretas = ['c', 'b', 'c', 'b', 'b'];
+            var resultadoDiv = document.getElementById('resultadoEng');
+            var materia = 'Eng'
             break;
         case 'bancoDados':
             respostasCorretas = ['b', 'b', 'c', 'b', 'a'];
+            var resultadoDiv = document.getElementById('resultadoBD');
+            var materia = 'BD'
             break;
         case 'poo':
             respostasCorretas = ['b', 'b', 'b', 'c', 'c'];
+            var resultadoDiv = document.getElementById('resultadoPOO');
+            var materia = 'POO'
             break;
     }
 
     for (var i = 1; i <= totalQuestoes; i++) {
-        var radios = document.getElementsByName('q' + i);
+        var radios = document.getElementsByName('q' + materia + i);
         for (var j = 0; j < radios.length; j++) {
             var radio = radios[j];
             if (radio.checked && radio.value === respostasCorretas[i - 1]) {
@@ -24,7 +45,22 @@ function verificarRespostas(materia) {
             }
         }
     }
-
-    var resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = 'Você acertou ' + acertos + ' de ' + totalQuestoes + ' questões.';
+
+    return acertos;
+}
+
+function exibirResultadosTotais() {
+    var acertosEngSoftware = verificarRespostas('engSoftware');
+    var acertosBancoDados = verificarRespostas('bancoDados');
+    var acertosPoo = verificarRespostas('poo');
+
+    var totalAcertos = acertosEngSoftware + acertosBancoDados + acertosPoo;
+    var totalQuestoes = 15;
+
+    var resultadoTotal = document.getElementById('resultado');
+    resultadoTotal.innerHTML = 'Você acertou ' + totalAcertos + ' de ' + totalQuestoes + ' questões no total.<br>' +
+        'Engenharia de Software: ' + acertosEngSoftware + ' de 5<br>' +
+        'Banco de Dados: ' + acertosBancoDados + ' de 5<br>' +
+        'POO: ' + acertosPoo + ' de 5';
 }
